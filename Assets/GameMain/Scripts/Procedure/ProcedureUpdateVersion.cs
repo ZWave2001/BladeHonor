eac99aa1f3040f869cf7ef3a79df648ebd2745cc
zxxxxjj <15279746341@163.com>
2023-09-19 17:51:32 +0800

    9.19 Company

--- Assets/GameMain/Scripts/Procedure/ProcedureUpdateVersion.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureUpdateVersion.cs
@@ -1,29 +1,64 @@
-﻿// Author: ZWave
-// Time: 2023/09/18 15:45
-// --------------------------------------------------------------------------
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
 
-using GameFramework.Fsm;
 using GameFramework.Procedure;
-
-
+using GameFramework.Resource;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 
 namespace BladeHonor
 {
     public class ProcedureUpdateVersion : ProcedureBase
     {
-        protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
+        private bool m_UpdateVersionComplete = false;
+        private UpdateVersionListCallbacks m_UpdateVersionListCallbacks = null;
+        
+
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+
+            m_UpdateVersionListCallbacks = new UpdateVersionListCallbacks(OnUpdateVersionListSuccess, OnUpdateVersionListFailure);
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
+
+            m_UpdateVersionComplete = false;
+
+            GameEntry.Resource.UpdateVersionList(procedureOwner.GetData<VarInt32>("VersionListLength"), procedureOwner.GetData<VarInt32>("VersionListHashCode"), procedureOwner.GetData<VarInt32>("VersionListCompressedLength"), procedureOwner.GetData<VarInt32>("VersionListCompressedHashCode"), m_UpdateVersionListCallbacks);
+            procedureOwner.RemoveData("VersionListLength");
+            procedureOwner.RemoveData("VersionListHashCode");
+            procedureOwner.RemoveData("VersionListCompressedLength");
+            procedureOwner.RemoveData("VersionListCompressedHashCode");
         }
 
-        protected override void OnUpdate(IFsm<IProcedureManager> procedureOwner, float elapseSeconds, float realElapseSeconds)
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
         {
             base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+
+            if (!m_UpdateVersionComplete)
+            {
+                return;
+            }
+
+            ChangeState<ProcedureVerifyResources>(procedureOwner);
         }
 
-        protected override void OnLeave(IFsm<IProcedureManager> procedureOwner, bool isShutdown)
+        private void OnUpdateVersionListSuccess(string downloadPath, string downloadUri)
         {
-            base.OnLeave(procedureOwner, isShutdown);
+            m_UpdateVersionComplete = true;
+            Log.Info("Update version list from '{0}' success.", downloadUri);
+        }
+
+        private void OnUpdateVersionListFailure(string downloadUri, string errorMessage)
+        {
+            Log.Warning("Update version list from '{0}' failure, error message is '{1}'.", downloadUri, errorMessage);
         }
     }
 }
--- Assets/GameMain/Scripts/Procedure/ProcedureVerifyResources.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureVerifyResources.cs
@@ -1,28 +1,71 @@
-﻿// Author: ZWave
-// Time: 2023/09/18 15:45
-// --------------------------------------------------------------------------
-
-using GameFramework.Fsm;
+﻿using GameFramework.Event;
 using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
 
 namespace BladeHonor
 {
     public class ProcedureVerifyResources : ProcedureBase
     {
-        protected override void OnEnter(IFsm<IProcedureManager> procedureOwner)
+        private bool m_VerifyResourcesComplete = false;
+
+ 
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
+
+            GameEntry.Event.Subscribe(ResourceVerifyStartEventArgs.EventId, OnResourceVerifyStart);
+            GameEntry.Event.Subscribe(ResourceVerifySuccessEventArgs.EventId, OnResourceVerifySuccess);
+            GameEntry.Event.Subscribe(ResourceVerifyFailureEventArgs.EventId, OnResourceVerifyFailure);
+
+            m_VerifyResourcesComplete = false;
+            GameEntry.Resource.VerifyResources(OnVerifyResourcesComplete);
         }
 
-        protected override void OnUpdate(IFsm<IProcedureManager> procedureOwner, float elapseSeconds, float realElapseSeconds)
-        {
-            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
-        }
-
-
-        protected override void OnLeave(IFsm<IProcedureManager> procedureOwner, bool isShutdown)
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
         {
             base.OnLeave(procedureOwner, isShutdown);
+
+            GameEntry.Event.Unsubscribe(ResourceVerifyStartEventArgs.EventId, OnResourceVerifyStart);
+            GameEntry.Event.Unsubscribe(ResourceVerifySuccessEventArgs.EventId, OnResourceVerifySuccess);
+            GameEntry.Event.Unsubscribe(ResourceVerifyFailureEventArgs.EventId, OnResourceVerifyFailure);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+
+            if (!m_VerifyResourcesComplete)
+            {
+                return;
+            }
+
+            ChangeState<ProcedureCheckResources>(procedureOwner);
+        }
+
+        private void OnVerifyResourcesComplete(bool result)
+        {
+            m_VerifyResourcesComplete = true;
+            Log.Info("Verify resources complete, result is '{0}'.", result);
+        }
+
+        private void OnResourceVerifyStart(object sender, GameEventArgs e)
+        {
+            ResourceVerifyStartEventArgs ne = (ResourceVerifyStartEventArgs)e;
+            Log.Info("Start verify resources, verify resource count '{0}', verify resource total length '{1}'.", ne.Count, ne.TotalLength);
+        }
+
+        private void OnResourceVerifySuccess(object sender, GameEventArgs e)
+        {
+            ResourceVerifySuccessEventArgs ne = (ResourceVerifySuccessEventArgs)e;
+            Log.Info("Verify resource '{0}' success.", ne.Name);
+        }
+
+        private void OnResourceVerifyFailure(object sender, GameEventArgs e)
+        {
+            ResourceVerifyFailureEventArgs ne = (ResourceVerifyFailureEventArgs)e;
+            Log.Warning("Verify resource '{0}' failure.", ne.Name);
         }
     }
 }
